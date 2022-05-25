5e0f164d817b7c8cd80864b73652c0954843f504
nuricovicyyanto <nuricovicyyanto@gmail.com>
2022-05-25 08:10:26 +0700

    Update admin.blade.php

--- resources/views/backend/layouts/admin.blade.php
+++ resources/views/backend/layouts/admin.blade.php
@@ -279,7 +279,7 @@
                 <div class="modal-body">Apakah anda yakin ingi Logout?</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="/">Logout</a>
+                    <a class="btn btn-primary" href="{{ url('logout') }}">Logout</a>
                 </div>
             </div>
         </div>
